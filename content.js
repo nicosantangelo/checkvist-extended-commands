f0f8aa6dee40cef1e37528f9766ed9fd254d3f76
Nicolás Santángelo <santangelonicolas@gmail.com>
2017-04-11 00:47:24 -0300

    Finish the expand and collapse methods

--- content.js
+++ content.js
@@ -14,20 +14,31 @@
 
 
   function bindCommands() {
+    var runTimeoutId = null
+    var nullTimeoutId = null
+
+    var getTaskId = function() { return maxkir.Task.selected_task_id() }
+
+    var actions = {
+      l: function() { maxkir.ChecklistNodeToggle.expand(getTaskId()) },
+      h: function() { maxkir.ChecklistNodeToggle.collapse(getTaskId()) }
+    }
+
     document.body.addEventListener('keyup', function(event) {
+      clearTimeout(runTimeoutId)
+      clearTimeout(nullTimeoutId)
+
       if (! maxkir.EditTracker.is_editing()) {
-        var selectedTaskId = maxkir.Task.selected_task_id()
+        var action = actions[event.key]
 
-        if (event.key === 'L') {
-          maxkir.ChecklistNodeToggle.expand(selectedTaskId)
-          event.preventDefault()
-        }
-
-        if (event.key === 'H') {
-          maxkir.ChecklistNodeToggle.collapse(selectedTaskId)
-          event.preventDefault()
+        if (action && runTimeoutId === null) {
+          runTimeoutId = setTimeout(action, 200)
         }
       }
+
+      nullTimeoutId = setTimeout(function() { runTimeoutId = null }, 250)
+
     }, false)
   }
+
 })()
