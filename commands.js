97e4dbf8becc071b6f629637d78e18ac588da070
Nicolás Santángelo <santangelonicolas@gmail.com>
2017-04-14 11:25:52 -0300

    Refactor a bit

--- commands.js
+++ commands.js
@@ -1,39 +1,48 @@
 ;(function() {
   'use strict'
 
-  var runTimeoutId = null
-  var nullTimeoutId = null
-
-  var getTaskId = function() { return maxkir.Task.selected_task_id() }
-
-  var actions = {
+  bindDelayed({
     l: function() { maxkir.ChecklistNodeToggle.expand(getTaskId()) },
     h: function() { maxkir.ChecklistNodeToggle.collapse(getTaskId()) },
+  })
 
+  bind({
     L: function() { maxkir.TaskFocus.set_focus(getTaskId()) },
     H: function() { maxkir.TaskFocus.remove_focus(getTaskId()) }
+  })
+
+
+  function bindDelayed(actions) {
+    var runTimeoutId = null
+    var nullTimeoutId = null
+
+    document.body.addEventListener('keyup', function(event) {
+      clearTimeout(runTimeoutId)
+      clearTimeout(nullTimeoutId)
+
+      if (! maxkir.EditTracker.is_editing()) {
+        var action = actions[event.key]
+
+        if (action && runTimeoutId === null) {
+          runTimeoutId = setTimeout(action, 200)
+        }
+      }
+
+      nullTimeoutId = setTimeout(function() { runTimeoutId = null }, 250)
+
+    }, false)
   }
 
-  document.body.addEventListener('keyup', function(event) {
-    clearTimeout(runTimeoutId)
-    clearTimeout(nullTimeoutId)
-
-    if (! maxkir.EditTracker.is_editing()) {
-      var action = actions[event.key]
-
-      if (action && runTimeoutId === null) {
-        runTimeoutId = setTimeout(action, 200)
+  function bind(actions) {
+    document.body.addEventListener('keyup', function(event) {
+      if (! maxkir.EditTracker.is_editing()) {
+        var action = actions[event.key]
+        if (action) action()
       }
-    }
+    })
+  }
 
-    nullTimeoutId = setTimeout(function() { runTimeoutId = null }, 250)
-
-  }, false)
-
-  document.body.addEventListener('keyup', function(event) {
-    if (! maxkir.EditTracker.is_editing()) {
-      var action = actions[event.key]
-      if (action) action()
-    }
-  })
+  function getTaskId() {
+    return maxkir.Task.selected_task_id()
+  }
 })()
