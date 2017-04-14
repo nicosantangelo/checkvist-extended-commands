9b739ba7e59de234a3ff5c28237d45e160fc2978
Nicolás Santángelo <santangelonicolas@gmail.com>
2017-04-14 11:20:32 -0300

    Extract the binding of commands to another script

---
+++ commands.js
@@ -0,0 +1,39 @@
+;(function() {
+  'use strict'
+
+  var runTimeoutId = null
+  var nullTimeoutId = null
+
+  var getTaskId = function() { return maxkir.Task.selected_task_id() }
+
+  var actions = {
+    l: function() { maxkir.ChecklistNodeToggle.expand(getTaskId()) },
+    h: function() { maxkir.ChecklistNodeToggle.collapse(getTaskId()) },
+
+    L: function() { maxkir.TaskFocus.set_focus(getTaskId()) },
+    H: function() { maxkir.TaskFocus.remove_focus(getTaskId()) }
+  }
+
+  document.body.addEventListener('keyup', function(event) {
+    clearTimeout(runTimeoutId)
+    clearTimeout(nullTimeoutId)
+
+    if (! maxkir.EditTracker.is_editing()) {
+      var action = actions[event.key]
+
+      if (action && runTimeoutId === null) {
+        runTimeoutId = setTimeout(action, 200)
+      }
+    }
+
+    nullTimeoutId = setTimeout(function() { runTimeoutId = null }, 250)
+
+  }, false)
+
+  document.body.addEventListener('keyup', function(event) {
+    if (! maxkir.EditTracker.is_editing()) {
+      var action = actions[event.key]
+      if (action) action()
+    }
+  })
+})()
--- content.js
+++ content.js
@@ -1,54 +1,10 @@
-;(function () {
+;(function() {
   'use strict'
-
-  // -----------------------------------------------------------------------------
-  // Start
-
   var script = document.createElement('script')
-  script.type = 'text/javascript'
 
-  script.textContent = bindCommands.toString()
-  script.textContent += '\nbindCommands()'
+  script.type  = 'text/javascript'
+  script.async = true
+  script.src   = chrome.extension.getURL('commands.js')
 
   document.body.appendChild(script)
-
-
-  function bindCommands() {
-    var runTimeoutId = null
-    var nullTimeoutId = null
-
-    var getTaskId = function() { return maxkir.Task.selected_task_id() }
-
-    var actions = {
-      l: function() { maxkir.ChecklistNodeToggle.expand(getTaskId()) },
-      h: function() { maxkir.ChecklistNodeToggle.collapse(getTaskId()) },
-
-      L: function() { maxkir.TaskFocus.set_focus(getTaskId()) },
-      H: function() { maxkir.TaskFocus.remove_focus(getTaskId()) }
-    }
-
-    document.body.addEventListener('keyup', function(event) {
-      clearTimeout(runTimeoutId)
-      clearTimeout(nullTimeoutId)
-
-      if (! maxkir.EditTracker.is_editing()) {
-        var action = actions[event.key]
-
-        if (action && runTimeoutId === null) {
-          runTimeoutId = setTimeout(action, 200)
-        }
-      }
-
-      nullTimeoutId = setTimeout(function() { runTimeoutId = null }, 250)
-
-    }, false)
-
-    document.body.addEventListener('keyup', function(event) {
-      if (! maxkir.EditTracker.is_editing()) {
-        var action = actions[event.key]
-        if (action) action()
-      }
-    })
-  }
-
 })()
