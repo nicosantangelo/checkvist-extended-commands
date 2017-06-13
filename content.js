e841d09eb4efa3f208b39bdfddf7532d7fdc9381
Nicolás Santángelo <santangelonicolas@gmail.com>
2017-06-13 22:01:31 +0200

    Make it look more like Workflowy

--- content.js
+++ content.js
@@ -7,4 +7,12 @@
   script.src   = chrome.extension.getURL('commands.js')
 
   document.body.appendChild(script)
+
+  var link = document.createElement('link')
+  link.media = 'screen'
+  link.rel   = 'stylesheet'
+  link.type  = 'text/css'
+  link.href  = chrome.extension.getURL('styles.css')
+
+  document.head.appendChild(link)
 })()
